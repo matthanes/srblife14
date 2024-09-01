93c7109a0e7dbe07e1f7f43d5e170b134bccfbf8
Matt Hanes <hanes.matt@gmail.com>
2024-09-01 11:24:39 -0400

    Add Events back to youth page

--- app/youth/page.tsx
+++ app/youth/page.tsx
@@ -8,45 +8,41 @@ import {
   getSplitScreens,
 } from '../../utils/directus';
 // import SplitScreen from '../components/SplitScreen';
-// import EventCardCarousel from '../components/EventCardCarousel';
-const announcements = await getAnnouncements();
+import EventCardCarousel from '@/components/EventCardCarousel/EventCardCarousel';
+let announcements = (await getAnnouncements()).data.announcements;
 const splitScreens = await getSplitScreens();
 
-const events = (await getAllEvents())
-  .filter((event) => new Date(event.datetime) > new Date())
-  .filter(
-    (event) => event.location === 'youth' || event.location === 'homeyouth',
-  );
+let events = (await getAllEvents()).filter((event) => {
+  return event.location === 'youth' || event.location === 'homeyouth';
+});
 
 export default function Youth() {
-  // const slides = announcements
-  //   .filter((announcement) => {
-  //     return (
-  //       announcement.location === 'youth' ||
-  //       announcement.location === 'homeyouth'
-  //     );
-  //   })
-  //   .map((announcement) => {
-  //     return {
-  //       url: announcement.slide_link || '',
-  //       ariaLabelText: announcement.link_label,
-  //       title: announcement.title,
-  //       subtitle: announcement.subtitle ?? '',
-  //       alt: announcement.alt_text,
-  //       imgLink:
-  //         'https://srblog.srblife.com/assets/' +
-  //         announcement.slide.filename_disk,
-  //       opacity: 0,
-  //       objectPosition: 'object-center',
-  //     };
-  //   });
-
-  const slides = [];
+  const slides = announcements
+    .filter((announcement) => {
+      return (
+        announcement.location === 'youth' ||
+        announcement.location === 'homeyouth'
+      );
+    })
+    .map((announcement) => {
+      return {
+        url: announcement.slide_link || '',
+        ariaLabelText: announcement.link_label,
+        title: announcement.title,
+        subtitle: announcement.subtitle ?? '',
+        alt: announcement.alt_text,
+        imgLink:
+          'https://srblog.srblife.com/assets/' +
+          announcement.slide.filename_disk,
+        opacity: 0,
+        objectPosition: 'object-center',
+      };
+    });
 
   return (
     <>
       <header className='relative min-h-1/3 sm:min-h-1/2 md:min-h-3/4 xl:min-h-screen'>
-        {/* <Slider slides={slides} timing={5000}></Slider> */}
+        <Slider slides={slides} timing={5000}></Slider>
       </header>
       <div className='container mx-auto my-8 px-4 md:px-12'>
         <div className='mb-12 flex flex-wrap justify-center gap-3 md:gap-6'>
@@ -104,18 +100,18 @@ export default function Youth() {
           body={splitScreen.body}
           reverse={splitScreen.reverse}
         />
-      ))}
+      ))} */}
 
       <h2 className="mx-auto mb-6 max-w-lg border-b-2 border-primary py-6 text-center font-bodytext text-4xl font-bold">
         Upcoming Events
       </h2>
-      {filteredEvents.length > 0 ? (
-        <EventCardCarousel events={filteredEvents} />
+      {events.length > 0 ? (
+        <EventCardCarousel events={events} />
       ) : (
         <div className="grid place-content-center p-6 text-xl">
           No upcoming events were found...
         </div>
-      )} */}
+      )}
     </>
   );
 }
