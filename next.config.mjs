dd81fbeb3f574409727665cfef6b5591a4a92b9c
Matt Hanes <hanes.matt@gmail.com>
2024-09-13 14:28:09 -0400

    config and lint changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  images: {
+    domains: ['srblog.srblife.com'],
+  },
 };
 
 export default nextConfig;
