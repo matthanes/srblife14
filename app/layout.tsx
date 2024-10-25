a495edff424505e3cdababe5e72aacb8331c681f
Matt Hanes <hanes.matt@gmail.com>
2024-10-25 11:17:47 -0400

    Try to switch to using bun

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,22 +8,29 @@ import Footer from '@/components/Footer/Footer';
 const poppins = Poppins({
   weight: ['400', '500', '700', '900', '600', '800'],
   subsets: ['latin'],
+  display: 'swap',
 });
 const roboto = Roboto({
   weight: ['400', '500', '700', '900'],
   subsets: ['latin'],
+  display: 'swap',
 });
 const robotoMono = Roboto_Mono({
   weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
+  display: 'swap',
 });
-const rockSalt = Rock_Salt({ weight: ['400'], subsets: ['latin'] });
+const rockSalt = Rock_Salt({
+  weight: ['400'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'Schomburg Road Baptist Church Columbus, Georgia',
   description:
     'Loving Him, loving them, changing lives! That is our purpose at Schomburg Road Baptist Church in Columbus, Georgia. Everything we do is driven from these six short words. By understanding this statement you will better understand who we are.',
-  keywords: 'church, family-friendly, baptist, small, kids, nursery'
+  keywords: 'church, family-friendly, baptist, small, kids, nursery',
 };
 
 export default function RootLayout({
--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -66,7 +66,7 @@ const BlogPost = async (props: BlogPostProps) => {
   return (
     <div className='bg-slate-100'>
       <article className='min-h-screen-nav p-4'>
-        <div className='relative w-full bg-white px-6 py-12 shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:mx-auto md:max-w-3xl lg:max-w-4xl lg:pb-28 lg:pt-16'>
+        <div className='w-full bg-white px-6 py-12 shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:mx-auto md:max-w-3xl lg:max-w-4xl lg:pb-28 lg:pt-16'>
           <div className='prose prose-stone mx-auto lg:prose-xl prose-h1:text-secondary prose-a:text-primary'>
             <h1 className='!text-4xl'>{title}</h1>
             <div className='mb-4 flex items-center justify-between border-y-2 border-secondary py-3'>
