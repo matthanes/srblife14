b389ef5519e51f6d752a45ed9b09fee262d86198
Matt Hanes <hanes.matt@gmail.com>
2025-05-10 14:43:59 -0400

    Remove height limit on event cards

--- components/EventCardCarousel/EventCard.tsx
+++ components/EventCardCarousel/EventCard.tsx
@@ -6,7 +6,7 @@ type EventCardProps = {
   event: SRBEvent;
 };
 
-const EventCard: React.FC<EventCardProps> = ({event}) => {
+const EventCard: React.FC<EventCardProps> = ({ event }) => {
   let {
     title,
     category,
@@ -26,25 +26,25 @@ const EventCard: React.FC<EventCardProps> = ({event}) => {
   });
 
   return (
-    <div className="mb-8 h-96 mx-auto flex w-full max-w-sm flex-col rounded-lg bg-white text-center shadow-lg lg:max-w-lg">
-      <div className="rounded-t-lg border-b-2 border-secondary border-opacity-50 bg-primary px-6 py-3 text-lg text-white">
+    <div className='mx-auto mb-8 flex w-full max-w-sm flex-col rounded-lg bg-white text-center shadow-lg lg:max-w-lg'>
+      <div className='border-secondary border-opacity-50 bg-primary rounded-t-lg border-b-2 px-6 py-3 text-lg text-white'>
         {category}
       </div>
-      <div className="flex grow flex-col p-6">
-        <h3 className="mb-2 text-xl font-medium leading-tight text-neutral-800">
+      <div className='flex grow flex-col p-6'>
+        <h3 className='mb-2 text-xl leading-tight font-medium text-neutral-800'>
           {title}
         </h3>
-        <p className="mb-4 text-base text-neutral-600">{short_description}</p>
+        <p className='mb-4 text-base text-neutral-600'>{short_description}</p>
         {button_link && button_text && (
           <Link
-            className="content mx-auto mt-auto w-fit rounded-lg border border-transparent bg-primary px-4 py-2 text-sm font-medium leading-5 text-white duration-150 hover:bg-opacity-90 focus-visible:outline-hidden focus-visible:ring-3 focus-visible:ring-secondary"
+            className='content bg-primary hover:bg-opacity-90 focus-visible:ring-secondary mx-auto mt-auto w-fit rounded-lg border border-transparent px-4 py-2 text-sm leading-5 font-medium text-white duration-150 focus-visible:ring-3 focus-visible:outline-hidden'
             href={button_link}
           >
             {button_text}
           </Link>
         )}
       </div>
-      <div className="border-t-2 border-neutral-100 px-6 py-3">
+      <div className='border-t-2 border-neutral-100 px-6 py-3'>
         <div>{datetime}</div>
         <div>{event_location}</div>
       </div>
